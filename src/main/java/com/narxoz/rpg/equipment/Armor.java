568bd7274dae460a01821a6db4c3da66f53d59bc
Bekzhan <bekzhan.tanirbergen@narxoz.kz>
2026-02-11 19:37:18 +0500

    add MedievalArmor and MagicArmor form Armor

--- src/main/java/com/narxoz/rpg/equipment/Armor.java
+++ src/main/java/com/narxoz/rpg/equipment/Armor.java
@@ -34,3 +34,28 @@ public class MedievalArmor implements Armor{
         return name + "type:" + type + "defense" + defense;
     }
 }
+public class MagicArmor implements Armor{
+    
+    private String name;
+    private int defense;
+    private String type;
+
+    public MagicArmor(){
+        this.name = "Mystic Rode";
+        this.defense = 20;
+        this.type = "Rode";
+    }
+
+    public int getDefense(){
+        return defense;
+    }
+    public String getArmorType(){
+        return type;
+    }
+    public String getArmorName(){
+        return name;
+    }
+    public String getArmorInfo(){
+        return name + "type:" + type + "defense" +defense; 
+    }
+} 
